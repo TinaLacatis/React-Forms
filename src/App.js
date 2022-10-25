6900d0d0bfacae3adb28908974b839546a25b9bd
Tina Lacatis <danatina.dt@gmail.com>
2022-10-25 08:37:03 +0000

    take the name is tapped in

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-// import { useState } from "react";
+import { useState } from "react";
 import "./styles.css";
 
-function handleChange(event) {
-  console.log(event.target.value);
-}
 export default function App() {
+  const [name, setName] = useState("");
+
+  function handleChange(event) {
+    // console.log(event.target.value);
+    setName(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {name}</h1>
       <input
         onChange={handleChange}
         type="text"

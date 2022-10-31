1c0feb80a8218082271db11f309827eabc7b4699
Tina Lacatis <danatina.dt@gmail.com>
2022-10-31 13:41:16 +0000

    on submit, new title appears

--- src/App.js
+++ src/App.js
@@ -3,21 +3,26 @@ import "./styles.css";
 
 export default function App() {
   const [name, setName] = useState("");
+  const [title, setTitle] = useState("");
 
   function handleChange(event) {
     // console.log(event.target.value);
     setName(event.target.value);
   }
+  function newTitle() {
+    setTitle(name);
+  }
 
   return (
     <div className="container">
-      <h1>Hello {name}</h1>
+      <h1>Hello {title}</h1>
       <input
         onChange={handleChange}
         type="text"
         placeholder="What's your name?"
+        value={name}
       />
-      <button>Submit</button>
+      <button onClick={newTitle}>Submit</button>
     </div>
   );
 }
